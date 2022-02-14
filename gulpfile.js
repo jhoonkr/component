e73b14abd3bcd4f367978c632b974acea023d5fc
JaeHoon Yu <dayofsoul@douzone.com>
2022-02-14 09:51:27 +0900

    Update: Gulpfile.js 수정

--- gulpfile.js
+++ gulpfile.js
@@ -5,95 +5,6 @@ var sourcemaps = require("gulp-sourcemaps");
 var cleancss = require("gulp-clean-css");
 var autoprefixer = require("gulp-autoprefixer");
 
-gulp.task("pf", function (done) {
-  gulp
-    .src("scss/*.scss")
-    .pipe(sourcemaps.init())
-    .pipe(
-      sass({ sourcemap: true, outputStyle: "expanded" }).on(
-        "error",
-        sass.logError
-      )
-    )
-    // .pipe(prefix({browser:["last 2 version", "> 1%", "ie 8", "ie 7"],cascade:false,flexbox:false}))
-    .pipe(
-      autoprefixer({
-        cascade: false,
-      })
-    )
-    .pipe(cleancss({ format: "keep-breaks" }))
-    .pipe(sourcemaps.write("./maps"))
-    .pipe(gulp.dest("./css"));
-  done();
-});
-
-gulp.task("pf:watch", function (done) {
-  gulp.watch("scss/*.scss", gulp.series("pf"));
-  gulp.watch("scss/common/*.scss", gulp.series("pf"));
-  gulp.watch("scss/components/*.scss", gulp.series("pf"));
-  done();
-});
-
-// portpolio
-
-gulp.task("pr", function (done) {
-  gulp
-    .src("portfolio/scss/*.scss")
-    .pipe(sourcemaps.init())
-    .pipe(
-      sass({ sourcemap: true, outputStyle: "expanded" }).on(
-        "error",
-        sass.logError
-      )
-    )
-    // .pipe(prefix({browser:["last 2 version", "> 1%", "ie 8", "ie 7"],cascade:false,flexbox:false}))
-    .pipe(
-      autoprefixer({
-        cascade: false,
-      })
-    )
-    .pipe(cleancss({ format: "keep-breaks" }))
-    .pipe(sourcemaps.write("./maps"))
-    .pipe(gulp.dest("./css"));
-  done();
-});
-
-gulp.task("pr:watch", function (done) {
-  gulp.watch("portfolio/scss/*.scss", gulp.series("pr"));
-  gulp.watch("portfolio/scss/common/*.scss", gulp.series("pr"));
-  gulp.watch("portfolio/scss/components/*.scss", gulp.series("pr"));
-  done();
-});
-
-gulp.task("newpf", function (done) {
-  gulp
-    .src("scss/*.scss")
-    .pipe(sourcemaps.init())
-    .pipe(
-      sass({ sourcemap: true, outputStyle: "expanded" }).on(
-        "error",
-        sass.logError
-      )
-    )
-    // .pipe(prefix({browser:["last 2 version", "> 1%", "ie 8", "ie 7"],cascade:false,flexbox:false}))
-    .pipe(
-      autoprefixer({
-        cascade: false,
-      })
-    )
-    .pipe(cleancss({ format: "keep-breaks" }))
-    .pipe(sourcemaps.write("./maps"))
-    .pipe(gulp.dest("./css"));
-  done();
-});
-
-gulp.task("newpf:watch", function (done) {
-  gulp.watch("scss/*.scss", gulp.series("newpf"));
-  gulp.watch("scss/common/*.scss", gulp.series("newpf"));
-  gulp.watch("scss/components/*.scss", gulp.series("newpf"));
-  done();
-});
-
 // SN
 
 gulp.task("sona", function (done) {
@@ -122,23 +33,5 @@ gulp.task("sona:watch", function (done) {
   gulp.watch("sona/scss/*.scss", gulp.series("sona"));
   gulp.watch("sona/scss/common/*.scss", gulp.series("sona"));
   gulp.watch("sona/scss/mixin/*.scss", gulp.series("sona"));
-  done();
-});
-
-// iOnYou
-
-// ionyou
-gulp.task('ionyou', function (done) {
-  gulp.src('ionyou/scss/scss/*.scss')
-      .pipe(sourcemaps.init())
-      .pipe(gulpsass({sourcemap: true, outputStyle: 'expanded'}).on('error', gulpsass.logError))
-      // .pipe(prefix({browser:["last 2 version", "> 1%", "ie 8", "ie 7"],cascade:false,flexbox:false}))
-      .pipe(cleancss({format: 'keep-breaks' }))
-      .pipe(sourcemaps.write('../maps'))
-      .pipe(gulp.dest('./ionyou/scss/css'));
-  done();
-});
-gulp.task('ionyou:watch', function (done) {
-  gulp.watch('ionyou/scss/*.scss', gulp.series('ionyou'));
   done();
 });
